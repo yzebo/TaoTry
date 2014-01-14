a996f44b4d35fb38c9a0bfeac19d69b6bbdab27c
Zebo <myzebo@gmail.com>
2014-01-14 21:33:39 +0800

    merge functions of handling different pages into one

--- javascripts/try.js
+++ javascripts/try.js
@@ -11,47 +11,29 @@ function editList(){
 }
 
 window.addEventListener('load', function(){
-    //taobao try homepage
     setTimeout(function(){
-        var items=document.querySelectorAll(".info .link");
-        if(items){
-            for(var i=0;i<items.length;i++){
-                items[i].href += '&from=taotry';
-                items[i].innerText += "√";
-            }
-        }
-    }, 1000);
-
-    //taobao try list
-    setTimeout(function(){
-        var scroller=document.querySelectorAll(".scroller .free-try");
-        if(scroller){
-            for(var i=0;i<scroller.length;i++){
-                scroller[i].href += '&from=taotry';
-                scroller[i].innerText += "√";
-            }
-        }
-        editList();
-    }, 1000);
-
-    //taobao try brand
-    setTimeout(function(){
-        var btn=document.querySelectorAll('.ui-goods-buy a');
-        if (btn) {
+        var items=document.querySelectorAll(".info .link");     //homepage
+        var scroller=document.querySelectorAll(".scroller .free-try");  //list page
+        var brand=document.querySelectorAll('.ui-goods-buy a');     //brand page
+        var btn=items.length?items:scroller;
+        btn=btn.length?btn:brand;
+        if(btn){
             for(var i=0;i<btn.length;i++){
                 btn[i].href += '&from=taotry';
                 btn[i].innerText += "√";
             }
-        };
+        }
+        if(scroller){
+            editList();
+            //try list next page
+            var count=0;
+            document.querySelector(".try-list-content").addEventListener("DOMNodeInserted", function(e) {
+                count++;
+                if(count==16){
+                    editList();
+                    count=0;
+                }
+            }, false);
+        }
     }, 1000);
 });
-
-//try list next page
-var count=0;
-document.querySelector(".try-list-content").addEventListener("DOMNodeInserted", function(e) {
-    count++;
-    if(count==16){
-        editList();
-        count=0;
-    }
-}, false);
