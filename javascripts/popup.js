c4b9816e49a92e0f55e276539b9c10155e1fadc0
Zebo <myzebo@gmail.com>
2014-03-08 20:42:47 +0800

    restore and save option

--- javascripts/popup.js
+++ javascripts/popup.js
@@ -8,9 +8,27 @@ function bindAction() {
         chrome.extension.getBackgroundPage().Login("http://try.taobao.com");
         return false;
     });
+    $('#inform').on('click',function(){
+        localStorage['inform'] = $('#inform').is(":checked");
+    });
 }
 
 function restoreOptions() {
+    var today=(new Date).getDate();
+    var lastday=localStorage['lastday'] || 0;
+    if(today==lastday){
+        $('#num').text(localStorage['num'] || 0);
+        if(parseInt($('#num').text())>=15){
+            $('small').text('今天申请次数已达上限');
+        }
+        else{
+            $('small').text('可以继续申请试用');
+        }
+    }
+    else{
+        $('#num').text(0);
+        $('small').text('今天还没申请试用');
+    }
     if (localStorage['inform'] == 'true') {
         $('#inform').prop('checked','checked');
     }
